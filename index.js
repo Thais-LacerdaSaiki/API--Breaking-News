5041407cb346c0ba47a4071922a6b0f6a28ac4f1
Thais <134336421+Thais-LacerdaSaiki@users.noreply.github.com>
2023-07-05 18:25:13 -0300

    refactor: add route soma

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+//ROTA
+    // METHOD HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
+    //método http é a forma como a internet se comunica entre cliente e servidor = req e res : requisição e resposta. O MÉTODO QUE ESTA SENDO UTILIZADO É O app que vem do "express" ponto.get (get é uma função)
+    // GET - PEGA UMA INFO (LEITURA - READ)
+    // POST - CRIA UMA INFO (CRIAÇÃO - CREATE)
+    // PUT - ALTERA TOSA A INFO (ATUALIZAÇÃO - UPDATE)
+    // PATH - ALTERA PARTE DA INFO (ATUALIZAÇÃO - UPDATE)
+    // DELETE - APAGA UMA INFO (DELETAR - DELETE)
+
+// NOME - Um identificador da rota
+
+// FUNCTION (CALLBACK) - é a  (=> arrow function - função anonima que recebe os parametros req e res) Responsável por executar um comando ela recebe a requisição e retorna uma resposta , nem sempre tem algum valor, pode até ter uma valor dentro do parametro ou dentro da rota.
+// Ela é executada por trás de outra função por isso recebe o nome de CALLBACK
+
+app.get("/soma", (req, res) => {
+    const soma = 100 + 1;
+    res.send({soma: soma}) 
 });
 
 app.listen(3000);
