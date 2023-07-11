cec3aeee3d3c0d4e205f9eeb8302334bef62498d
Thais <134336421+Thais-LacerdaSaiki@users.noreply.github.com>
2023-07-11 00:28:32 -0300

    connecting mongodb

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
+const connectDatabase = require("./src/database/db");
 
-const userRoute = require('./src/routes/user.route');
+const userRoute = require("./src/routes/user.route");
 
 
 const port = 3000;
 
+connectDatabase("mongodb+srv://root:root@cluster0.kuid43b.mongodb.net/?retryWrites=true&w=majority");
 app.use(express.json());
 
 app.use('/user', userRoute);
 
 app.listen(port, () => console.log ('Servidor rodando na porta ${port}'));
-
